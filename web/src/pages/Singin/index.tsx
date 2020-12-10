a59bfe09749b704e81dad97728386472c9cc257d
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-12-10 20:14:01 -0300

    Isolando componentes

--- web/src/pages/Singin/index.tsx
+++ web/src/pages/Singin/index.tsx
@@ -1,23 +1,31 @@
 import React from "react";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 
 import { Container, Content, Background } from "./styles";
 
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
 const SingIn: React.FC = () => (
   <Container>
     <Content>
+      <img src={logoImg} alt="GoBarber" />
       <form action="">
-        <img src={logoImg} alt="GoBarber" />
         <h1>Faça seu login</h1>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} type="email" placeholder="email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="senha"
+        />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci minha senha</a>
       </form>
 
-      <a href="">
+      <a href="login">
         <FiLogIn />
         Criar conta
       </a>

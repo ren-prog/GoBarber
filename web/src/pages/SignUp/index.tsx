a28c7a323fb0418287fbc0e554bb831373b07608
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-12-10 21:26:01 -0300

    Utilizando Unform

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FiMail, FiLock, FiUser, FiArrowLeft } from "react-icons/fi";
-
+import { Form } from "@unform/web";
 import logoImg from "../../assets/logo.svg";
 
 import { Container, Content, Background } from "./styles";
@@ -8,30 +8,35 @@ import { Container, Content, Background } from "./styles";
 import Input from "../../components/Input";
 import Button from "../../components/Button";
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background></Background>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
-      <form action="">
-        <h1>Faça seu cadastro</h1>
-        <Input name="name" icon={FiUser} type="email" placeholder="Nome" />
-        <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
-        <Input
-          name="password"
-          icon={FiLock}
-          type="password"
-          placeholder="senha"
-        />
-        <Button type="submit">Cadastrar</Button>
-      </form>
+const SignUp: React.FC = () => {
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+  return (
+    <Container>
+      <Background></Background>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+          <Input name="email" icon={FiMail} type="email" placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="senha"
+          />
+          <Button type="submit">Cadastrar</Button>
+        </Form>
 
-      <a href="login">
-        <FiArrowLeft />
-        Voltar
-      </a>
-    </Content>
-  </Container>
-);
+        <a href="login">
+          <FiArrowLeft />
+          Voltar
+        </a>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;

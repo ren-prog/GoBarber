4a722404e002e62eca979e83218d0814a92fe133
Renan Albuquerque <62337593+ren-prog@users.noreply.github.com>
2020-12-22 21:49:43 -0300

    Dependency inversion Principle

---
+++ server/src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -0,0 +1,31 @@
+import { getRepository, Repository } from "typeorm";
+import Appointment from "../entities/Appointment";
+
+import IAppointmentRepository from "@modules/appointments/repositories/IAppointmentRepository";
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
+
+class AppointmentRepository implements IAppointmentRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = await this.ormRepository.findOne({
+      where: { date: date },
+    });
+    return findAppointment;
+  }
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentRepository;
--- server/src/modules/appointments/services/CreateAppointmentService.ts
+++ server/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,30 +1,29 @@
 import { startOfHour } from "date-fns";
 
-import { getCustomRepository } from "typeorm";
 import Appointment from "../infra/typeorm/entities/Appointment";
-import AppointmentRepository from "../repositories/AppointmentRepository";
 
 import AppError from "@shared/errors/AppError";
+
+import IAppointmentRepository from "../repositories/IAppointmentRepository";
 /** Correção
  * [x] Recebimento das informações
  * [x] Trativa de erros
  * [x] Acesso ao repositorio
  */
 
-interface RequestDTO {
+interface IRequestDTO {
   provider_id: string;
   date: Date;
 }
 class CreateAppointmentService {
+  constructor(private appointmentRepository: IAppointmentRepository) {}
   public async execute({
     date,
     provider_id,
-  }: RequestDTO): Promise<Appointment> {
-    const appointmentRepository = getCustomRepository(AppointmentRepository);
-
+  }: IRequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentRepository.findByDate(
       appointmentDate
     );
 
@@ -32,12 +31,10 @@ class CreateAppointmentService {
       throw new AppError("This appointment is already booked");
     }
 
-    const appointment = appointmentRepository.create({
+    const appointment = await this.appointmentRepository.create({
       date: appointmentDate,
       provider_id,
     });
-
-    await appointmentRepository.save(appointment);
 
     return appointment;
   }

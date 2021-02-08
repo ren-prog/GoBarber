f6a182fcfdfb5ca06b257e1b9802083c8d5af755
Renan <prog.renan@gmail.com>
2021-02-08 18:55:20 -0300

    Template engine

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -18,21 +18,21 @@ app.use(express.json());
 app.use("/files", express.static(uploadConfig.uploadsFolder));
 app.use(routes);
 
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: "error",
-        message: err.message,
-      });
-    }
+// app.use(
+//   (err: Error, request: Request, response: Response, next: NextFunction) => {
+//     if (err instanceof AppError) {
+//       return response.status(err.statusCode).json({
+//         status: "error",
+//         message: err.message,
+//       });
+//     }
 
-    return response.status(500).json({
-      status: "error",
-      message: "Internal server error",
-    });
-  }
-);
+//     return response.status(500).json({
+//       status: "error",
+//       message: "Internal server error",
+//     });
+//   }
+// );
 
 app.listen(3333, () => {
   console.log("Rodando na porta 3333");

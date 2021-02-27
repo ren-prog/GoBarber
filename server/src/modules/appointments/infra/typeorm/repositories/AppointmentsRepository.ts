fc53c8493ab597cc2f49d47cb62a4834c57e9a7d
Renan <prog.renan@gmail.com>
2021-02-26 23:27:23 -0300

    Filtrando agendamentos por mês

--- server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ server/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,8 +1,9 @@
-import { getRepository, Repository } from "typeorm";
+import { getRepository, Repository, Raw } from "typeorm";
 import Appointment from "../entities/Appointment";
 
 import IAppointmentRepository from "@modules/appointments/repositories/IAppointmentsRepository";
 import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
+import IFindAllInMonthFromProviderDTO from "@modules/appointments/dtos/IFindAllInMonthFromProviderDTO";
 
 class AppointmentRepository implements IAppointmentRepository {
   private ormRepository: Repository<Appointment>;
@@ -25,6 +26,25 @@ class AppointmentRepository implements IAppointmentRepository {
     await this.ormRepository.save(appointment);
 
     return appointment;
+  }
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const parsedMonth = String(month).padStart(2, "0");
+
+    const appointments = await this.ormRepository.find({
+      where: {
+        provider_id,
+        date: Raw(
+          (dateFieldName) =>
+            `to_char(${dateFieldName}, MM-YYYY) = '${parsedMonth}-${year}'`
+        ),
+      },
+    });
+
+    return appointments;
   }
 }
 
--- server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ server/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { v4 as uuid } from "uuid";
-import { isEqual } from "date-fns";
+import { isEqual, getMonth, getYear } from "date-fns";
 
 import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO";
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointment";
 import IAppointmentsRepository from "../IAppointmentsRepository";
+import IFindAllInMonthFromProviderDTO from "@modules/appointments/dtos/IFindAllInMonthFromProviderDTO";
 
 export default class FakeAppointmentsRepository
   implements IAppointmentsRepository {
@@ -17,6 +18,20 @@ export default class FakeAppointmentsRepository
     return findAppointment;
   }
 
+  public async findAllInMonthFromProvider({
+    provider_id,
+    month,
+    year,
+  }: IFindAllInMonthFromProviderDTO): Promise<Appointment[]> {
+    const appointments = this.appointments.filter(
+      (appointment) =>
+        appointment.provider_id === provider_id &&
+        getMonth(appointment.date) + 1 === month &&
+        getYear(appointment.date) === year
+    );
+
+    return appointments;
+  }
   public async create({
     provider_id,
     date,

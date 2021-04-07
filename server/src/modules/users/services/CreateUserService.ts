3717f1cd4ff6c8ef4f0665164cefbd1c0a1c8cd7
Renan <prog.renan@gmail.com>
2021-04-07 14:44:01 -0300

    Invalidando cache

--- server/src/modules/users/services/CreateUserService.ts
+++ server/src/modules/users/services/CreateUserService.ts
@@ -5,6 +5,8 @@ import AppError from "@shared/errors/AppError";
 import IUsersRepository from "../repositories/IUsersRepository";
 import IHashProvider from "../providers/hashProvider/models/IHashProvider";
 
+import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider";
+
 interface IRequestDTO {
   name: string;
   email: string;
@@ -17,7 +19,9 @@ class CreateUserService {
     @inject("UsersRepository")
     private usersRepository: IUsersRepository,
     @inject("HashProvider")
-    private hashProvider: IHashProvider
+    private hashProvider: IHashProvider,
+    @inject("CacheProvider")
+    private cacheProvider: ICacheProvider
   ) {}
 
   public async execute({ name, email, password }: IRequestDTO): Promise<User> {
@@ -34,6 +38,8 @@ class CreateUserService {
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix("providers-list");
 
     return user;
   }

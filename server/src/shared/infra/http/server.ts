d112f34b7cd71e1db4963b78d38754d5bcd0c147
Renan <prog.renan@gmail.com>
2021-04-07 22:43:38 -0300

    Express rate limit

--- server/src/shared/infra/http/server.ts
+++ server/src/shared/infra/http/server.ts
@@ -9,12 +9,14 @@ import routes from "./routes";
 import uploadConfig from "@config/upload";
 
 import AppError from "@shared/errors/AppError";
+import rateLimiter from "./middleware/rateLimiter";
 
 import "@shared/infra/typeorm";
 import "@shared/container";
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use("/files", express.static(uploadConfig.uploadsFolder));

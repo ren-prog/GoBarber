df6c0b3a58afd1460e8ee9e37773f4158fa38816
Renan <prog.renan@gmail.com>
2021-04-14 10:52:37 -0300

    Enviando formulário a API

--- web/src/pages/ForgotPassword/index.tsx
+++ web/src/pages/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { FiLogIn, FiMail } from "react-icons/fi";
 import { FormHandles } from "@unform/core";
 import { Form } from "@unform/web";
@@ -14,12 +14,14 @@ import Input from "../../components/Input";
 import Button from "../../components/Button";
 
 import { Container, Content, AnimationContainer, Background } from "./styles";
+import api from "../../services/api";
 
 interface ForgotPasswordFormData {
   email: string;
 }
 
 const ForgotPassword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
 
   const { addToast } = useToast();
@@ -40,7 +42,16 @@ const ForgotPassword: React.FC = () => {
         });
 
         // recuperação de senha
+        await api.post("/password/forgot", {
+          email: data.email,
+        });
 
+        addToast({
+          type: "success",
+          title: "E-mail de recuperação enviado",
+          description:
+            "Enviamos um e-mail para confirmar a recuperação de senha, cheque sua caixa de entrada",
+        });
         // history.push('/dashboard');
       } catch (err) {
         if (err instanceof Yup.ValidationError) {
@@ -57,6 +68,8 @@ const ForgotPassword: React.FC = () => {
           description:
             "Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.",
         });
+      } finally {
+        setLoading(false);
       }
     },
     [addToast]
@@ -73,7 +86,9 @@ const ForgotPassword: React.FC = () => {
 
             <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-            <Button type="submit">Recuperar</Button>
+            <Button loading={loading} type="submit">
+              Recuperar
+            </Button>
           </Form>
 
           <Link to="/">
